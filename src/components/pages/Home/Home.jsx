cfb4a29270dd7f0c4a06fb13f0b06d8ae1498c74
Maite Martinez <ssanchez005@misena.edu.co>
2023-02-27 17:54:50 -0500

    Modificación de archivos

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,32 +1,62 @@
-import React, { useState } from "react";  
+import React, { useState, useEffect, useRef } from "react";
 import '../../../index.css'
+
 export const Home = () => {
-  const [text, setText] = useState()
-  const TextArea = useRef('null')
+  const [text, setText] = useState('');
+  const [archivedTweets, setArchivedTweets] = useState([]);
+  const [textLimitReached, setTextLimitReached] = useState();
+  const TextArea = useRef(null);
+  const [showArchived, setShowArchived] = useState(false);
 
   const handleText = () => {
-    setText(TextArea.current.value)
+    const currentValue = TextArea.current.value;
+    setText(currentValue);
+    if (currentValue.length > 255) {
+      setTextLimitReached(true);
+    } else {
+      setTextLimitReached(false);
+    }
   }
-  useEffect(() => {
-  if(text === 'Hola'){
-    setText('Chao')
+
+  const handleArchive = () => {
+    if (text !== '') {
+      setArchivedTweets([archivedTweets, text]);
+      setText('');
+    }
   }
-},[text])
+  const handlePublish = () => {
+    setPublishedText(text);
+    setText('');
+    setCounter(255);
+  }
+
+const counter = 255 - text.length;
   return (
     <>
-    {/* <h1>Generador de Tweests</h1> */}
-    <div className="content">
-    <h2 className="title">Publique su tweet</h2>
-    <textarea ref={TextArea} placeholder="Comience a escribir" className="textAreaTweet"></textarea>
-    <p className="Response">{text}</p>
-    <button onClick={handleText} className="public">Publicar</button>
-    <button className="archived">Archivar</button>
-    <button className="showArchive">Mostrar Archivos</button>
-    <p className='counter'>255</p>
-    <span className="tweetsArchived">Aquí verá sus tweets archivados</span>
-    </div>
+      <div className="content">
+        <h2 className="title">Publique su tweet</h2>
+        <textarea
+  ref={TextArea}
+  placeholder="Comience a escribir"
+  className="textAreaTweet"
+  disabled={textLimitReached}
+  maxLength={255} 
+  onInput ={handleText}
+></textarea>
+        <p className="Response">{text}</p>
+        <button onClick={handlePublish} className="public">Publicar</button>
+        <button onClick={handleArchive} className="archived">Archivar</button>
+        <button onClick={() => setShowArchived(true)} className="showArchive">Mostrar Archivos</button>
+        <p className='counter'>{counter}</p>
+        <span className="tweetsArchived">Aquí verá sus tweets archivados</span>
+        {showArchived && (
+          <div className="archivedTweets" placeholder="Aquí verá sus tweets archivados">
+            {archivedTweets.map((tweet, index) => (
+              <p key={index}>{tweet}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
-
-
